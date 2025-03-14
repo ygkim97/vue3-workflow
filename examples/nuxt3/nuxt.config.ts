9456e6106573e19e723729c2be3e2df9a4f67667
ygkim97 <dnjdn21@naver.com>
2025-03-14 11:52:10 +0900

    feat: 파일 변경시 자동 리로드 처리 - hmr 활성화

--- examples/nuxt3/nuxt.config.ts
+++ examples/nuxt3/nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-})
+  vite: {
+    server: {
+      hmr: true
+    }
+  }
+});
--- vite.config.ts
+++ vite.config.ts
@@ -4,21 +4,24 @@ import { resolve } from "node:path";
 
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    hmr: true
+  },
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.js"),
       name: "VueFlowCore",
-      fileName: (format) => `vue-flow-core.${format}.min.js`,
+      fileName: (format) => `vue-flow-core.${format}.min.js`
     },
     rollupOptions: {
       external: ["vue"], // Vue를 외부 모듈로 설정
       output: {
         globals: {
-          vue: "Vue",
+          vue: "Vue"
         },
-        exports: "named", // UMD에서 named export 사용
-      },
+        exports: "named" // UMD에서 named export 사용
+      }
     },
-    minify: "terser", // UMD 파일 크기 최적화
-  },
+    minify: "terser" // UMD 파일 크기 최적화
+  }
 });
