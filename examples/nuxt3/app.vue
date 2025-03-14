6bd2cbdcb40ffc3ad21718e10e6707eceb8a88b2
ygkim97 <dnjdn21@naver.com>
2025-03-14 14:35:25 +0900

    feat: id 설정 및 nodes, edges 렌더링 기능 추가 - VueFlowCore 에 id 설정 및 nodes, edges 렌더링 기능 추가 - Nuxt 에 테스트 구현

--- examples/nuxt3/app.vue
+++ examples/nuxt3/app.vue
@@ -1,10 +1,57 @@
 <template>
-  <div>
-    <h1>VueFlowCore</h1>
-    <VueFlowCore/>
+  <div class="work-flow">
+    <header>Nuxt - Work Flow Test</header>
+    <VueFlowCore id="nuxtVueFlow" :nodes="nodes" :edges="edges" />
   </div>
 </template>
 
 <script lang="ts" setup>
+import { MarkerType } from "@vue-flow/core";
 
+const nodes = ref([
+  {
+    id: "test1",
+    type: "input",
+    position: { x: 100, y: 100 },
+    data: { label: "Start" }
+  },
+  {
+    id: "test2",
+    type: "default",
+    position: { x: 400, y: 100 },
+    data: { label: "Process" }
+  }
+]);
+const edges = ref([
+  {
+    id: "e1-2",
+    type: "step",
+    source: "test1",
+    target: "test2",
+    markerEnd: MarkerType.ArrowClosed
+  }
+]);
 </script>
+
+<style>
+html,
+body,
+#__nuxt {
+  margin: 0;
+  height: 100%;
+}
+
+.work-flow {
+  display: grid;
+  height: 100%;
+  grid-template-rows: 50px auto;
+}
+
+header {
+  background-color: black;
+  color: white;
+  display: flex;
+  align-items: center;
+  padding: 20px;
+}
+</style>
--- src/components/vue-flow-core.vue
+++ src/components/vue-flow-core.vue
@@ -1,13 +1,43 @@
 <template>
-  <div>
-    vue-flow-core
-  </div>
+  <VueFlow :id="id" :nodes="props.nodes" :edges="props.edges"> </VueFlow>
 </template>
 
 <script lang="ts" setup>
-console.log("vue-flow-core");
+import { VueFlow } from "@vue-flow/core";
+import type { PropType } from "vue";
+import type { Node, Edge } from "@vue-flow/core";
+import GraphData1 from "../graph-data/graph-data-1.json";
+
+const props = defineProps({
+  id: {
+    type: String,
+    default: "vueFlowCore"
+  },
+  nodes: {
+    type: Array as PropType<Node[]>,
+    default: () => {
+      // TODO: 개발하기 위해 default 데이터 설정, 개발 완료 후 삭제
+      return GraphData1.nodes;
+    }
+  },
+  edges: {
+    type: Array as PropType<Edge[]>,
+    default: () => {
+      // TODO: 개발하기 위해 default 데이터 설정, 개발 완료 후 삭제
+      return GraphData1.edges;
+    }
+  }
+});
 </script>
 
-<style lang="scss" scoped>
+<style>
+@import "@vue-flow/core/dist/style.css";
+@import "@vue-flow/core/dist/theme-default.css";
 
+html,
+body,
+#app {
+  margin: 0;
+  height: 100%;
+}
 </style>
